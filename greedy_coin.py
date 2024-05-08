46b09720f5a18ecb312db022933135a1368beadb
Matthias Schmidt <matthias.schmidt.92@gmx.de>
2024-05-08 19:55:32 +0200

    added greedy coin

---
+++ greedy_coin.py
@@ -0,0 +1,21 @@
+def greedy_coin(amount:int, coins:list) -> tuple:
+    """
+    Algorithm taking a amound to pay and a list of avaiblable coins.
+    Returns the total number of coins and the list of coins 
+    needed to match the amount.
+    """
+    coins.sort(reverse=True)
+    coin_list = []
+    coins_total = 0
+    for coin in coins:
+        while coin <= amount:
+            coin_list.append(coin)
+            amount -= coin
+            coins_total += 1
+    return coins_total, coin_list
+
+
+if __name__ == "__main__":
+    amount = 97
+    coins = [50, 10, 5, 2, 1]
+    print(greedy_coin(amount, coins))
